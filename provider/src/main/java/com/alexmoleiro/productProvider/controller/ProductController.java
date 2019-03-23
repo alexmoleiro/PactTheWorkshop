bd4ffcab1aef18cb37062a4915800a2179da19c3
A.Moleiro <alejandro.moleiro@schibsted.com>
2019-03-24 00:00:03 +0100

    Provider is verified at its minimum

--- provider/src/main/java/com/alexmoleiro/productProvider/controller/ProductController.java
+++ provider/src/main/java/com/alexmoleiro/productProvider/controller/ProductController.java
@@ -4,10 +4,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/products/1")
 public class ProductController {
 
-    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getProduct() {
         return "{\"productName\":\"iphoneX\"}";
     }
--- provider/src/test/java/com/alexmoleiro/productProvider/ProductProviderApplicationTests.java
+++ provider/src/test/java/com/alexmoleiro/productProvider/ProductProviderApplicationTests.java
@@ -1,19 +1,34 @@
 package com.alexmoleiro.productProvider;
 
 import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.RestPactRunner;
+import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
-import au.com.dius.pact.provider.spring.SpringRestPactRunner;
-import org.junit.Test;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.target.MockMvcTarget;
+import com.alexmoleiro.productProvider.controller.ProductController;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
-//@RunWith(SpringRestPactRunner.class)
-@RunWith(SpringRestPactRunner.class)
-@Provider("productListProvider")
+@RunWith(RestPactRunner.class)
+@Provider("productoProvider")
 @PactFolder("pacts")
 public class ProductProviderApplicationTests {
 
-	@Test
+	@TestTarget
+	public final MockMvcTarget target = new MockMvcTarget();
+
+	@Before
+	public void setup() {
+
+		ProductController productController = new ProductController();
+		target.setControllers(productController);
+
+	}
+
+	@State("user id")
 	public void verifiesPact() {
+		target.setRunTimes(1);
 	}
 
 }
