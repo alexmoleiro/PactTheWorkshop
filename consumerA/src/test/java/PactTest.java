416b2d07b4b5e7a4224e25c1c95b3bb66fdf85bb
A.Moleiro <alejandro.moleiro@schibsted.com>
2019-03-23 16:52:38 +0100

    Readme and first test

---
+++ consumerA/src/test/java/PactTest.java
@@ -0,0 +1,10 @@
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PactTest {
+    @Test
+    public void shouldStartWorking() {
+        Assert.assertThat(1, Is.is(1));
+    }
+}
